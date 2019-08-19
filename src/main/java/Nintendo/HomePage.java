bca0ac07c8e61bb3325dc21ffbe661dff436170a
anotherchester <anotherchester@gmail.com>
2019-08-18 19:33:34 -0700

    updated selectors

--- src/main/java/Nintendo/HomePage.java
+++ src/main/java/Nintendo/HomePage.java
@@ -8,7 +8,7 @@ public class HomePage extends BasePage {
 
     public HomePage (WebDriver driver) {super(driver);}
 
-    @FindBy(css = "#page-container > div > div > div.footer.aem-GridColumn.aem-GridColumn--default--12 > footer > div:nth-child(1) > div > div > div > nav > div > ul > li:nth-child(1) > ul > li:nth-child(2) > a")
+    @FindBy(css = "a[href^=\"https://careers.nintendo.com\"]")
     private WebElement careersButton;
 
     public CareersPage clickCareersButton() {
